61434a19458287abd67988c7ae5e1b5d9cc3600e
Idoko Augustine <linkwithaugustine@gmail.com>
2025-08-16 23:10:53 -0700

    setup updated

--- applai/src/App.jsx
+++ applai/src/App.jsx
@@ -1,27 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from './components/Layout/Layout';
-import Dashboard from './pages/Dashboard';
-import Applications from './pages/Applications';
-import Interviews from './pages/Interviews';
-import Login from './pages/Login';
+import React from 'react';
 
-function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-
-                {/* Routes with layout*/}
-                <Route element={<Layout />}>
-                    <Route path="/dashboard" element={<Dashboard />} />
-                    <Route path="/applications" element={<Applications />} />
-                    <Route path="/interviews" element={<Interviews />} />
-                </Route>
-
-                {/* Routes without layout */}
-                <Route path="/login" element={<Login />} />
-            </Routes>
-        </BrowserRouter>
-    );
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  )
 }
 
 export default App;
