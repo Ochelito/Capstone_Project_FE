82a1c9ff762e5cdee3cef9f7e5d110146059911d
Idoko Augustine <linkwithaugustine@gmail.com>
2025-08-28 08:04:17 -0700

    bug fix: removed duplicate login button on login page

--- applai/src/components/Forms/LoginForm.jsx
+++ applai/src/components/Forms/LoginForm.jsx
@@ -45,9 +45,7 @@ export default function LoginForm() {
         onChange={(e) => setPassword(e.target.value)}
         className="p-2 border rounded"
       />
-      <button type="submit" className="px-4 py-2 bg-blue-600 text-white rounded-lg">
-        Login
-      </button>
+     
     </form>
   );
 }
